5c0ef5e7640cdd8e853e0b30909e065064dce4bf
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-04-07 18:12:08 +0200

    Implement first messages and their serialization
    Add some test to check the serialization stuff
    Next part is to automaticly encode / decode when exchanging messages

---
+++ include/network/msg.hh
@@ -0,0 +1,144 @@
+#ifndef _NETWORK_MSG_HH_
+#define _NETWORK_MSG_HH_
+
+#include <string>
+
+#include <boost/asio.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/serialization/serialization.hpp>
+
+namespace hyper {
+	namespace network {
+
+		struct request_name
+		{
+			private:
+				friend class boost::serialization::access;
+				template<class Archive>
+				void serialize(Archive & ar, const unsigned int version)
+				{
+					(void) version;
+					ar & name;
+				}
+
+			public:
+				std::string name;
+		};
+
+		struct request_name_answer
+		{
+			private:
+				friend class boost::serialization::access;
+				template<class Archive>
+				void save(Archive & ar, const unsigned int version) const
+				{
+					(void) version;
+					unsigned short port;
+					std::string addr;
+					ar & name;
+					ar & success;
+					if (success) {
+						addr = endpoint.address().to_string();
+						port =  endpoint.port();
+
+						ar & addr;
+						ar & port;
+					}
+				}
+
+				template <class Archive>
+				void load(Archive & ar, const unsigned int version)
+				{
+					(void) version;
+					ar & name;
+					ar & success;
+					if (success) {
+						std::string addr;
+						boost::asio::ip::address address;
+						unsigned short port;
+
+						ar & addr;
+						ar & port;
+
+						address = boost::asio::ip::address::from_string(addr);
+						endpoint = boost::asio::ip::tcp::endpoint(address, port);
+					}
+				}
+
+				BOOST_SERIALIZATION_SPLIT_MEMBER()
+
+			public:
+				std::string name;
+				bool success;
+				boost::asio::ip::tcp::endpoint endpoint;
+		};
+
+		struct register_name
+		{
+			private:
+				friend class boost::serialization::access;
+				template<class Archive>
+				void save(Archive & ar, const unsigned int version) const
+				{
+					(void) version;
+					std::string addr;
+					unsigned short port;
+					ar & name;
+
+					addr = endpoint.address().to_string();
+					port = endpoint.port();
+
+					ar & addr;
+					ar & port;
+				}
+
+				template<class Archive>
+				void load(Archive & ar, const unsigned int version)
+				{
+					(void) version;
+					ar & name;
+
+					std::string addr;
+					boost::asio::ip::address address;
+					unsigned short port;
+
+					ar & addr;
+					ar & port;
+
+					address = boost::asio::ip::address::from_string(addr);
+					endpoint = boost::asio::ip::tcp::endpoint(address, port);
+				}
+
+				BOOST_SERIALIZATION_SPLIT_MEMBER()
+			public:
+				std::string name;
+				boost::asio::ip::tcp::endpoint endpoint;
+		};
+
+		struct register_name_answer
+		{
+			private:
+				friend class boost::serialization::access;
+				template<class Archive>
+				void serialize(Archive & ar, const unsigned int version)
+				{
+					(void) version;
+					ar & name;
+					ar & success;
+				}
+			public:
+				std::string name;
+				bool success;
+		};
+
+		typedef boost::mpl::vector<
+			request_name,
+			request_name_answer,
+			register_name,
+			register_name_answer
+		> message_types;
+
+	};
+};
+
+#endif /* _NETWORK_MSG_HH_ */
---
+++ test/test_network_msg.cc
@@ -0,0 +1,70 @@
+#include <network/msg.hh>
+#include <boost/test/unit_test.hpp>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+template <typename T>
+void try_archive_interface(const T& src, T& dest)
+{
+	std::string tmp;
+	{
+		std::ostringstream archive_stream;
+		boost::archive::text_oarchive archive(archive_stream);
+		archive << src;
+		tmp = archive_stream.str();
+	}
+
+	{ 
+		std::istringstream archive_stream(tmp);
+		boost::archive::text_iarchive archive(archive_stream);
+		archive >> dest;
+	}
+}
+
+
+BOOST_AUTO_TEST_CASE ( network_msg_test )
+{
+
+	using namespace hyper::network;
+
+	struct request_name request_name1, request_name2;
+	request_name1.name = "myAbility";
+
+	try_archive_interface(request_name1, request_name2);
+
+	BOOST_CHECK(request_name1.name == request_name2.name);
+
+	struct request_name_answer request_name_answer1, request_name_answer2;
+	request_name_answer1.success = true;
+	request_name_answer1.name = "myAbility";
+	request_name_answer1.endpoint = boost::asio::ip::tcp::endpoint(
+										boost::asio::ip::address::from_string("127.0.0.1"), 4242);
+
+	try_archive_interface(request_name_answer1, request_name_answer2);
+
+	BOOST_CHECK(request_name_answer1.name == request_name_answer2.name);
+	BOOST_CHECK(request_name_answer1.success == request_name_answer2.success);
+	BOOST_CHECK(request_name_answer1.endpoint == request_name_answer2.endpoint);
+
+
+	struct register_name register_name1, register_name2;
+	register_name1.name = "myAbility";
+	register_name1.endpoint = boost::asio::ip::tcp::endpoint(
+							  boost::asio::ip::address::from_string("227.0.52.1"), 4242);
+
+	try_archive_interface(register_name1, register_name2);
+
+	BOOST_CHECK(register_name1.name == register_name2.name);
+	BOOST_CHECK(register_name1.endpoint == register_name2.endpoint);
+
+	struct register_name_answer register_name_answer1, register_name_answer2;
+	register_name_answer1.name = "otherAbility";
+	register_name_answer1.success = false;
+
+	try_archive_interface(register_name_answer1, register_name_answer2);
+
+	BOOST_CHECK(register_name_answer1.name == register_name_answer2.name);
+	BOOST_CHECK(register_name_answer1.success == register_name_answer2.success);
+}
+
