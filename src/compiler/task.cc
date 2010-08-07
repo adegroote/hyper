5b83d5e76c4a3ba8fe657a93c74fa0fb54c633a8
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-08-07 19:01:45 +0200

    Transform a compiler::expression_ast into a parsable logic expr

--- src/compiler/task.cc
+++ src/compiler/task.cc
@@ -155,6 +155,98 @@ namespace {
 			return oss.str();
 		}
 	};
+	
+	template <binary_op_kind op> struct logic_function {};
+	template <> struct logic_function<ADD> { static std::string name() { return "add";}};
+	template <> struct logic_function<SUB> { static std::string name() { return "sub";}};
+	template <> struct logic_function<MUL> { static std::string name() { return "mul";}};
+	template <> struct logic_function<DIV> { static std::string name() { return "div";}};
+	template <> struct logic_function<EQ> { static std::string name() { return "equal";}};
+	template <> struct logic_function<NEQ>{ static std::string name() { return "not_equal";}};
+	template <> struct logic_function<LT> { static std::string name() { return"less";}};
+	template <> struct logic_function<LTE>{ static std::string name() { return "less_or_equal";}};
+	template <> struct logic_function<GT> { static std::string name() { return"greater";}};
+	template <> struct logic_function<GTE>{ static std::string name() { return "geater_or_equal";}};
+	template <> struct logic_function<AND>{ static std::string name() { return "and";}};
+	template <> struct logic_function<OR> { static std::string name() { return "or";}};
+
+	template <unary_op_kind op> struct logic_unary_function {};
+	template <> struct logic_unary_function<NEG> { static std::string name() { return "neg";}};
+	
+	struct dump_logic_expression_ast  : public boost::static_visitor<std::string>
+	{
+		std::string operator() (const empty& e) const { (void)e; assert(false); }
+
+		template <typename T>
+		std::string operator() (const Constant<T>& c) const
+		{
+			std::ostringstream oss;
+			oss << c.value;
+			return oss.str();
+		}
+
+		std::string operator() (const Constant<bool>& c) const
+		{
+			std::ostringstream oss;
+			if (c.value)
+				oss << "true";
+			else 
+				oss << "false";
+			return oss.str();
+		}
+
+		std::string operator() (const std::string& s) const
+		{
+			return s;
+		}
+
+		std::string operator() (const function_call& f) const
+		{
+			std::ostringstream oss;
+			(void) f;
+			oss << f.fName << "(";
+			for (size_t i = 0; i < f.args.size(); ++i) {
+				oss << boost::apply_visitor(dump_logic_expression_ast(), f.args[i].expr);
+				if (i != (f.args.size() - 1)) {
+					oss << ",";
+				}
+			}
+			oss << ")";
+			return oss.str();
+		}
+
+		std::string operator() (const expression_ast& e) const
+		{
+			std::ostringstream oss;
+			oss << "(";
+			oss << boost::apply_visitor(dump_logic_expression_ast(), e.expr);
+			oss << ")";
+			return oss.str();
+		}
+
+		template <binary_op_kind T>
+		std::string operator() (const binary_op<T> & op) const
+		{
+			std::ostringstream oss;
+			oss << logic_function<T>::name();
+			oss << "(";
+			oss << boost::apply_visitor(dump_logic_expression_ast(), op.left.expr);
+			oss << ",";
+			oss << boost::apply_visitor(dump_logic_expression_ast(), op.right.expr); 
+			oss << ")";
+			return oss.str();
+		}
+
+		template <unary_op_kind T>
+		std::string operator() (const unary_op<T>& op) const
+		{
+			std::ostringstream oss;
+			oss << "(";
+			oss << boost::apply_visitor(dump_logic_expression_ast(), op.subject.expr);
+			oss << ")";
+			return oss.str();
+		}
+	};
 
 	struct is_remote_sym 
 	{
