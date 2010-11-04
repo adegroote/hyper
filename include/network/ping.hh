df58df8ff87635103b66270b09daa5c3260a0025
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-11-04 19:14:43 +0100

    Implement a periodic process which sends ping message

---
+++ include/network/ping.hh
@@ -0,0 +1,43 @@
+#ifndef HYPER_NETWORK_PING_HH_
+#define HYPER_NETWORK_PING_HH_
+
+#include <network/client_tcp_impl.hh>
+
+namespace hyper {
+	namespace network {
+		class ping_process {
+
+			/* 
+			 * ping_process can throw a boost::system::system_error exception
+			 * if something fails (in particular, if the remote host dies).
+			 */
+			public:
+				explicit ping_process(boost::asio::io_service& io_s,
+							 boost::posix_time::time_duration delay,
+							 const std::string& destName,
+							 const std::string destPort);
+
+				void run();
+
+				void stop();
+
+			private:
+
+			  void handle_timeout(const boost::system::error_code&);
+			  void handle_connect(const boost::system::error_code&);
+			  void handle_write(const boost::system::error_code&);
+
+			  bool is_connected;
+			  std::string destName_;
+			  std::string destPort_;
+
+			  boost::asio::io_service& io_service_;
+			  boost::posix_time::time_duration delay_;
+			  network::tcp::client<boost::mpl::vector<> > c_;
+			  boost::asio::deadline_timer deadline_;
+			  ping msg_;
+		};
+	}
+}
+
+#endif /* HYPER_NETWORK_PING_HH_ */
---
+++ src/network/ping.cc
@@ -0,0 +1,72 @@
+#include <network/ping.hh>
+
+#include <boost/system/system_error.hpp>
+
+using namespace hyper::network;
+
+ping_process::ping_process(boost::asio::io_service& io_s,
+						   boost::posix_time::time_duration delay,
+						   const std::string& destName,
+						   const std::string destPort) :
+	is_connected(false),
+	destName_(destName),
+	destPort_(destPort),
+	io_service_(io_s),
+	delay_(delay),
+	c_(io_service_),
+	deadline_(io_service_) 
+{}
+
+void ping_process::handle_write(const boost::system::error_code& e)
+{
+	if (e) {
+		throw boost::system::system_error(e);
+	} else {
+		/* Rearm the timer, and let's go for another loop */
+		run();
+	}
+}
+
+void ping_process::handle_connect(const boost::system::error_code& e)
+{
+	if (e) {
+		throw boost::system::system_error(e);
+	} else {
+		is_connected = true;
+		msg_.value++;
+		c_.async_write(msg_ , boost::bind(&ping_process::handle_write,
+									  this,
+									  boost::asio::placeholders::error));
+	}
+}
+
+void ping_process::handle_timeout(const boost::system::error_code& e)
+{
+	if (e) {
+		throw boost::system::system_error(e);
+	} else {
+		if (!is_connected) {
+			c_.async_connect(destName_, destPort_, 
+							 boost::bind(&ping_process::handle_connect,
+										 this,
+										 boost::asio::placeholders::error));
+		} else {
+			handle_connect(e);
+		}
+	}
+}
+
+void ping_process::run()
+{
+	deadline_.expires_from_now(delay_);
+	deadline_.async_wait(boost::bind(&ping_process::handle_timeout, 
+									  this,
+									  boost::asio::placeholders::error));
+}
+
+void ping_process::stop()
+{
+	deadline_.cancel();
+}
+
+
