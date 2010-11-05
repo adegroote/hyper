b9b89026e8568c40dbfd1dae9d2916555b7536b0
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-11-05 11:11:32 +0100

    Don't use the ns_visitor concept and ns::map_addr anymore for runtime
    The implementation is not really usable, locking inside the class is a
    bad thing and it is hard to augment information stored in the map. This
    stuff will be used only in hyperruntime, so don't bother do write some
    bad abstraction here :)
    
    No function changes expected

--- src/hyperruntime/hyperruntime.cc
+++ src/hyperruntime/hyperruntime.cc
@@ -15,16 +15,64 @@ namespace details {
 	typedef boost::make_variant_over<input_msg>::type input_variant;
 	typedef boost::make_variant_over<output_msg>::type output_variant;
 
+	struct ability_context 
+	{
+		network::ns::addr_storage addr;
+		int timeout_occurence;
+	};
+	
+	typedef std::map<std::string, ability_context> runtime_map;
+
 	struct runtime_visitor : public boost::static_visitor<output_variant>
 	{
-		network::tcp::ns_visitor& ns_vis;
+		runtime_map &map;
+		boost::shared_mutex& m;
+		network::tcp::ns_port_generator& gen;
 
-		runtime_visitor(network::tcp::ns_visitor& ns_vis_) : ns_vis(ns_vis_) {}
+		runtime_visitor(runtime_map& map_, boost::shared_mutex& m_,
+						network::tcp::ns_port_generator& gen_) :
+			map(map_), m(m_), gen(gen_) 
+		{}
 
-		template <typename T>
-		output_variant operator() (const T& t) const
+		output_variant operator() (const network::request_name& r) const
 		{
-			return ns_vis(t);
+			std::cerr << "receiving name request : " << r << std::endl;
+
+			boost::shared_lock<boost::shared_mutex> lock(m);
+			runtime_map::iterator it = map.find(r.name);
+
+			network::request_name_answer res_msg;
+			res_msg.name = r.name;
+			res_msg.success = (it != map.end());
+			if (it == map.end()) 
+				res_msg.endpoint = it->second.addr.tcp_endpoint;
+
+			std::cerr << "answering to name request : " << res_msg << std::endl;
+			return res_msg;
+		}
+
+		output_variant operator() (const network::register_name& r) const
+		{
+			using namespace boost::asio;
+
+			std::cerr << "receiving name register request : " << r << std::endl;
+			ability_context ctx;
+			ctx.addr.tcp_endpoint = ip::tcp::endpoint(ip::address_v4::any(), gen.get());
+
+			boost::upgrade_lock<boost::shared_mutex> lock(m);
+			runtime_map::iterator it = map.find(r.name);
+			bool already_here = (it != map.end());
+			if (!already_here) {
+				map.insert(std::make_pair(r.name, ctx));
+			}
+
+			network::register_name_answer res_msg;
+			res_msg.name = r.name;
+			res_msg.endpoint = ctx.addr.tcp_endpoint;
+			res_msg.success = !already_here;
+
+			std::cerr << "answering to name register request : " << res_msg << std::endl;
+			return res_msg;
 		}
 
 		output_variant operator() (const network::ping& p) const
@@ -39,10 +87,11 @@ namespace details {
 int main()
 {
 	boost::asio::io_service io_s;
-	network::ns::map_addr map;
+
+	boost::shared_mutex m;
+	details::runtime_map map;
 	network::tcp::ns_port_generator gen("5000");
-	network::tcp::ns_visitor ns_vis(map, gen, std::cout);
-	details::runtime_visitor runtime_vis(ns_vis);
+	details::runtime_visitor runtime_vis(map, m, gen);
 
 	typedef network::tcp::server<details::input_msg, 
 								  details::output_msg, 
