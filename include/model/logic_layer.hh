93eb80f59845af125b786eb31c447836ebcc8b38
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-11-09 13:04:54 +0100

    Deal with remote constraint : first part
    From the remote constraint, generate a logic constraint and ask the
    system if the constraint is currently true or not.
    At the moment, it doesn't do lots of things, as the engine is empty.

--- include/model/logic_layer.hh
+++ include/model/logic_layer.hh
@@ -4,6 +4,7 @@
 #include <boost/bind.hpp>
 #include <boost/thread/thread.hpp>
 
+#include <logic/engine.hh>
 #include <model/concurrent_queue.hh>
 
 namespace hyper {
@@ -24,9 +25,12 @@ namespace hyper {
 				ability& a_;
 				logic_queue& queue_;
 				bool abort;
+				logic::engine& engine_;
 
-				logic_solver(logic_queue& queue__, ability& a) :
-					a_(a), queue_(queue__), abort(false)
+				logic_solver(logic_queue& queue__, ability& a,
+						logic::engine& engine) :
+					a_(a), queue_(queue__), abort(false),
+					engine_(engine)
 				{}
 
 				void run();
@@ -35,12 +39,14 @@ namespace hyper {
 
 		struct logic_layer {
 			logic_queue& queue_;
+			logic::engine engine;
 			details::logic_solver solver;
 			boost::thread thr;
 
 			explicit logic_layer(logic_queue& queue, ability &a) :
 				queue_(queue),
-				solver(queue, a),
+				engine(),
+				solver(queue, a, engine),
 				thr(boost::bind(&details::logic_solver::run, &solver))
 			{}
 
--- src/model/logic_layer.cc
+++ src/model/logic_layer.cc
@@ -1,22 +1,44 @@
+#include <logic/expression.hh>
+
 #include <model/ability.hh>
+#include <model/execute_impl.hh>
 #include <model/logic_layer.hh>
 
 namespace hyper {
 	namespace model {
 		namespace details {
-				void logic_solver::run()
-				{
-					logic_constraint ctr;
-					while (! abort)  {
-						queue_.wait_and_pop(ctr);
-						
-						if (abort) 
-							return;
+			void logic_solver::run()
+			{
+				logic_constraint ctr;
+				while (! abort)  {
+					queue_.wait_and_pop(ctr);
 
-						std::cout << "Handling constraint : " ;
-						std::cout << ctr.constraint << std::endl;
+					if (abort) 
+						return;
+
+					std::cout << "Handling constraint : " ;
+					std::cout << ctr.constraint << std::endl;
+
+					logic::generate_return ret =
+						logic::generate(ctr.constraint, engine_.funcs());
+
+					if (ret.res == false) {
+						std::cerr << "Can't parse constraint" << std::endl;
+						continue;
 					}
+
+					boost::optional<bool> b =
+						model::evaluate_expression<bool>(a_.io_s, ret.e, a_);
+
+					if (b && *b) {
+						std::cout << "Constraint is already enforced, doing nothing";
+						std::cout << std::endl;
+						continue;
+					}
+
+					std::cout << "Time to do something smart" << std::endl;
 				}
+			}
 		}
 	}
 }
