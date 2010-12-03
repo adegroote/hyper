fe9f8f510052b8c0ee559abd5c2cfff2bb860606
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-12-03 13:05:01 +0100

    actor_client is a client to handle data exchanges between agents
    The main idea is to have an actor_client for each agent, and use them to
    exchange message. The answer will come on the "standard interface" of
    the agent, and will fire callback when receiving new answer.

---
+++ include/network/actor_protocol.hh
@@ -0,0 +1,281 @@
+#ifndef HYPER_NETWORK_ACTOR_PROTOCOL_HH
+#define HYPER_NETWORK_ACTOR_PROTOCOL_HH
+
+#include <string>
+#include <map>
+
+#include <boost/function.hpp>
+#include <boost/mpl/assert.hpp>
+#include <boost/mpl/end.hpp>
+#include <boost/mpl/find.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/void.hpp>
+#include <boost/optional.hpp>
+#include <boost/variant/variant.hpp>
+#include <boost/variant/get.hpp>
+
+#include <network/msg.hh>
+#include <network/nameserver.hh>
+#include <network/client_tcp_impl.hh>
+
+namespace hyper {
+	namespace network {
+		/* InputM is a mpl sequence of acceptable input type */
+		template <typename InputM>
+		class callback_database 
+		{
+			public:
+				typedef boost::function<void ()> fun_cb;
+				typedef typename boost::make_variant_over<InputM>::type msg_variant;
+
+			private:
+				struct cb_info {
+					fun_cb cb;
+					msg_variant input;
+					std::string actor_dst;
+				};
+
+				typedef std::set<identifier> set_id;
+
+				std::map<identifier, cb_info> map_cb;
+				std::map<std::string, set_id> id_by_actor;
+
+				void remove_helper(identifier id)
+				{
+					map_cb.erase(id);
+				}
+
+
+			public:
+				void add(const std::string& actor, identifier id, fun_cb cb)
+				{
+					cb_info info;
+					info.cb = cb;
+					info.actor_dst = actor;
+
+					map_cb[id] = info;
+					id_by_actor[actor].insert(id);
+				}
+				
+				template <typename T>
+				void trigger(identifier id, const T& input)
+				{
+					using namespace boost::mpl;
+					typedef typename find<InputM, T>::type index;
+					typedef typename end<InputM>::type index_end;
+					BOOST_MPL_ASSERT_NOT(( boost::is_same< index, index_end > ));
+
+					cb_info& info = map_cb[id];
+					info.input = input;
+					info.cb();
+				}
+
+				template <typename T>
+				T get_input(identifier id)
+				{
+					cb_info& info = map_cb[id];
+					return boost::get<T>(info.input);
+				}
+
+				void remove(identifier id)
+				{
+					typename std::map<identifier, cb_info>::iterator it;
+					it = map_cb.find(id);
+					assert(it != map_cb.end());
+
+					id_by_actor[it->second.actor_dst].erase(id);
+					remove_helper(id);
+				}
+
+				void cancel(const std::string& actor)
+				{
+					set_id& s = id_by_actor[actor];
+					std::for_each(s.begin(), s.end(), 
+								  boost::bind(&callback_database::remove_helper, this, _1));
+				}
+		};
+
+		/* Actor must have :
+		 *	- a name (std::string)
+		 *	- a boost::asio::io_service io_s, running
+		 *	- a method identifier gen_identifier()
+		 *	- a callback_database db to deal with async request
+		 *	- a resolver name_client
+		 */
+
+		/* actor_protocol_visitor only accepts message with an id field */
+		template <typename Actor>
+		struct actor_protocol_visitor : public boost::static_visitor<boost::mpl::void_>
+		{
+			Actor& actor;
+			actor_protocol_visitor(Actor& actor_) : actor(actor_) {}
+
+			template <typename T>
+			boost::mpl::void_ operator() (const T& t) const
+			{
+				identifier id = t.id;
+				actor.db.trigger(id, t);
+				return boost::mpl::void_();
+			}
+		};
+
+		template <typename Actor>
+		class actor_client
+		{
+			private:
+				Actor& actor;
+				tcp::client<boost::mpl::vector<> > c_;
+				bool connected;
+				std::string actor_dst;
+				name_resolve solver;
+
+				template <typename Handler>
+				void handle_basic_write(const boost::system::error_code& e,
+										boost::tuple<Handler> handler)
+				{
+					if (e)
+						connected = false;
+					boost::get<0>(handler) (e);
+				}
+
+				template <typename Handler>
+				void handle_write(const boost::system::error_code& e,
+									identifier id, 
+									boost::tuple<Handler> handler)
+				{
+					if (e)
+					{
+						actor.db.remove(id);
+						boost::get<0>(handler) (e);
+					}
+				}
+
+				template <typename Output, typename Handler>
+				void handle_request(
+									 identifier id,
+									 Output& output,
+									 boost::tuple<Handler> handler)
+				{
+					boost::system::error_code e;
+					try {
+						 output =  actor.db.template get_input<Output>(id);
+					} catch (const boost::bad_get&)
+						{
+							e  = boost::asio::error::invalid_argument;
+						}
+					actor.db.remove(id);
+					boost::get<0>(handler)(e);
+				}
+
+				template <typename Input, typename Handler>
+				void write(const Input& input, boost::tuple<Handler> handler)
+				{
+					void (actor_client::*f) (const boost::system::error_code&,
+							boost::tuple<Handler>) =
+						&actor_client::template handle_basic_write<Handler>;
+
+					c_.async_write(input,
+							boost::bind(f, this,
+										   boost::asio::placeholders::error, handler));
+				}
+
+				template <typename Input, typename Handler>
+				void handle_connect(const boost::system::error_code& e,
+									const Input& input,
+									boost::tuple<Handler> handler)
+				{
+					if (e) 
+						boost::get<0>(handler)(e);
+					else {
+						connected=true;
+						write (input, handler);
+					}
+				}
+
+				template <typename Input, typename Handler>
+				void handle_resolve(const boost::system::error_code& e,
+									const Input& input,
+									boost::tuple<Handler> handler)
+				{
+					if (e) 
+						boost::get<0>(handler)(e);
+					else {
+						void (actor_client::*f) (const boost::system::error_code&,
+								const Input& input,
+								boost::tuple<Handler>) =
+							&actor_client::template handle_connect<Input, Handler>;
+
+						c_.async_connect(solver.endpoint(), 
+							boost::bind(f, this, 
+										boost::asio::placeholders::error, 
+										boost::cref(input),
+										handler));
+					}
+				}
+
+				template <typename Input, typename Handler>
+				void async_write_(const Input& input, boost::tuple<Handler> handler)
+				{
+					if (!connected) {
+					void (actor_client::*f) (const boost::system::error_code&,
+							const Input& input,
+							boost::tuple<Handler>) =
+						&actor_client::template handle_resolve<Input, Handler>;
+					solver.name(actor_dst);
+					
+					actor.name_client.async_resolve(solver,
+							boost::bind(f, this, 
+										 boost::asio::placeholders::error,
+										 boost::cref(input),
+										 handler));
+					} else {
+						write(input, handler);
+					}
+				}
+
+
+			public:
+				actor_client(Actor& actor_, const std::string& dst) :
+					actor(actor_), c_(actor.io_s), connected(false), actor_dst(dst)
+				{}
+
+				template <typename Input, typename Handler>
+				void async_write(const Input& input, Handler handler)
+				{
+					return async_write_(input, boost::make_tuple(handler));
+				}
+
+				template <typename Input, typename Output, typename Handler>
+				void async_request(const Input& input, 
+								   Output& output, Handler handler)
+				{
+					input.id = actor.gen_identifier();
+					input.src = actor.name;
+
+					void (actor_client::*write_cb)(
+							const boost::system::error_code& e,
+							identifier id,
+							boost::tuple<Handler>)
+						= &actor_client::template handle_write<Handler>;
+
+					void (actor_client::*request_cb)(
+							identifier id,
+							Output &,
+							boost::tuple<Handler>)
+						= &actor_client::template handle_request<Output, Handler>;
+
+					actor.db.add(actor.name, input.id, 
+								 boost::bind(request_cb,
+											 this, input.id, boost::ref(output),
+											 boost::make_tuple(handler)));
+
+					async_write(input, boost::bind(write_cb,
+												 this, 
+												 boost::asio::placeholders::error,
+												 input.id, boost::make_tuple(handler)));
+				}
+		};
+	}
+}
+
+#endif /* HYPER_NETWORK_ACTOR_PROTOCOL_HH */
---
+++ test/test_network_actor_protocol.cc
@@ -0,0 +1,140 @@
+#include <network/actor_protocol.hh>
+#include <network/msg.hh>
+#include <boost/test/unit_test.hpp>
+
+#include <boost/thread.hpp>
+
+using namespace hyper::network;
+
+namespace {
+	typedef boost::mpl::vector<request_constraint> input_serv;
+	typedef boost::mpl::vector<boost::mpl::void_> output_serv;
+	typedef boost::mpl::vector<request_constraint_answer> input_client;
+	typedef boost::mpl::vector<boost::mpl::void_> output_client;
+	typedef boost::make_variant_over< input_serv>::type input_serv_variant;
+	typedef boost::make_variant_over< output_serv>::type output_serv_variant;
+	typedef boost::make_variant_over< input_client>::type input_client_variant;
+	typedef boost::make_variant_over< output_client>::type output_client_variant;
+
+	struct false_resolv
+	{
+		false_resolv() {};
+
+		template <typename Handler>
+		void async_resolve(name_resolve & solv, Handler handler)
+		{
+			if (solv.name() == "client") {
+			solv.rna.endpoint = boost::asio::ip::tcp::endpoint(
+					  boost::asio::ip::address::from_string("127.0.0.1"), 5000);
+			} else {
+			solv.rna.endpoint = boost::asio::ip::tcp::endpoint(
+					  boost::asio::ip::address::from_string("127.0.0.1"), 5001);
+			}
+			handler(boost::system::error_code());
+		}
+	};
+
+	struct simple_agent {
+		size_t identifier;
+		std::string name;
+		boost::asio::io_service io_s;
+		false_resolv name_client;
+		callback_database<input_client> db;
+
+		size_t gen_identifier() { return identifier++; }
+	};
+
+	typedef hyper::network::tcp::server<input_client, output_client, actor_protocol_visitor<simple_agent>		>
+		client_serv;
+
+	void dont_care(const boost::system::error_code& ) {}
+
+	struct simple_server_visitor : public boost::static_visitor<output_serv_variant>
+	{
+		request_constraint_answer ans;
+		actor_client<simple_agent> & client;
+
+		simple_server_visitor(actor_client<simple_agent> & client_) : client(client_) {}
+
+		boost::mpl::void_ operator() (const request_constraint& r) 
+		{
+			ans.id = r.id;
+			ans.success = true;
+			client.async_write(ans, dont_care);
+
+			return boost::mpl::void_();
+		}
+	};
+
+	typedef hyper::network::tcp::server<input_serv, output_serv, simple_server_visitor>
+		server_serv;
+
+	struct agent_client {
+		simple_agent agent;
+		actor_client<simple_agent> client;
+		actor_protocol_visitor<simple_agent> vis;
+		client_serv serv;
+		int count_valid_test;
+		request_constraint req;
+		request_constraint_answer ans;
+
+		agent_client() : client(agent, "server"), 
+			vis(agent), serv("127.0.0.1", "5000", vis, agent.io_s) {
+			agent.name = "client";
+			count_valid_test = 0;
+		}
+
+		void handle_test(const boost::system::error_code& e)
+		{
+			BOOST_CHECK(!e);
+			BOOST_CHECK(ans.id == 42);
+			BOOST_CHECK(ans.success == true);
+			count_valid_test ++;
+		}
+
+		void stop() { serv.stop(); }
+
+		void test_async()
+		{
+			agent.identifier = 42;
+			req.constraint = "test";
+			client.async_request(req, ans, boost::bind(&agent_client::handle_test, this,
+										boost::asio::placeholders::error));
+		}
+	};
+
+	struct agent_serv {
+		simple_agent agent;
+		actor_client<simple_agent> client;
+		simple_server_visitor vis;
+		server_serv serv;
+
+		agent_serv() : client(agent, "client"),
+			vis(client),
+			serv("127.0.0.1", "5001", vis, agent.io_s) {
+				agent.name = "server";
+			}
+
+		void stop() { serv.stop(); }
+	};
+}
+
+BOOST_AUTO_TEST_CASE ( network_actor_protocol_test )
+{
+	agent_serv serv;
+	agent_client client;
+
+	boost::thread thr_serv( boost::bind(& boost::asio::io_service::run, &serv.agent.io_s));
+	boost::thread thr_client( boost::bind(& boost::asio::io_service::run, &client.agent.io_s));
+
+	client.test_async();
+	// sleep a bit to be sure that everything happens
+	boost::this_thread::sleep(boost::posix_time::milliseconds(10)); 
+
+	serv.stop();
+	client.stop();
+	thr_serv.join();
+	thr_client.join();
+
+	BOOST_CHECK(client.count_valid_test == 1);
+}
