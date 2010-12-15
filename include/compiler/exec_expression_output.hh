10996a73718476b2309a25e9ff0250f925939f10
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-12-15 18:47:20 +0100

    Add some overloads for exec_expression_output using some recipe&

--- include/compiler/exec_expression_output.hh
+++ include/compiler/exec_expression_output.hh
@@ -4,12 +4,15 @@
 #include <iostream>
 #include <string>
 
+#include <boost/optional.hpp>
+
 namespace hyper {
 	namespace compiler {
 		struct universe;
 		struct ability;
 		struct task;
 		struct typeList;
+		struct recipe;
 
 
 		struct exec_expression_output
@@ -17,6 +20,7 @@ namespace hyper {
 			const universe& u;
 			const ability& ability_context;
 			const task& task_context;
+			boost::optional<const recipe&> recipe_context;
 			std::ostream& oss;
 			const typeList& tList;
 			size_t counter;
@@ -33,6 +37,26 @@ namespace hyper {
 				u(u_),
 				ability_context(ability_context_),
 				task_context(task_context_),
+				recipe_context(boost::none),
+				oss(oss_), 
+				tList(tList_),
+				counter(0),
+				num_conds(num_conds_),
+				base_expr(base_expr_)
+			{}
+
+			exec_expression_output(const universe& u_,
+					const ability& ability_context_, 
+					const task& task_context_,
+					const recipe& recipe_context_,
+					std::ostream& oss_,
+					const typeList& tList_,
+					size_t num_conds_,
+					const std::string& base_expr_):
+				u(u_),
+				ability_context(ability_context_),
+				task_context(task_context_),
+				recipe_context(recipe_context_),
 				oss(oss_), 
 				tList(tList_),
 				counter(0),
