336ae41ffc42cab8664862a810b9c1a5ca1b7697
Arnaud Degroote <arnaud.degroote@laas.fr>
2011-02-17 16:24:06 +0100

    Introduce abortable_function and abortable_computation
    The second one represents a sequence of function (reprensented by the
    first one), chained by continuation model. Moreover, as expected by
    their name, they could be aborted.
    
    It will be used to implement recipes

---
+++ include/model/abortable_function.hh
@@ -0,0 +1,106 @@
+#ifndef HYPER_MODEL_ABORTABLE_FUNCTION_HH_
+#define HYPER_MODEL_ABORTABLE_FUNCTION_HH_
+
+#include <vector>
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
+namespace hyper {
+	namespace model {
+
+		void none_function() {}
+
+		struct abortable_function_base {
+			typedef boost::function<void (const boost::system::error_code& e)> cb_type;
+			virtual void compute (cb_type cb) = 0;
+			virtual void abort() = 0 ;
+			virtual ~abortable_function_base() {};
+		};
+
+		struct abortable_function : public abortable_function_base {
+			typedef boost::function<void (cb_type)> exec_type;
+			typedef boost::function<void (void)> abort_type;
+
+			exec_type exec_;
+			abort_type abort_;
+
+			abortable_function(exec_type exec, abort_type abort = none_function):
+				exec_(exec), abort_(abort) {}
+
+			void compute (cb_type cb) {
+				return exec_(cb);
+			}
+
+			void abort() {
+				return abort_();
+			}
+		};
+
+		/* 
+		 * Abortable_computation represents a sequence of function, chained by
+		 * a continuation model (a continuation monad in other term).
+		 *
+		 * The computation will fail if any element of the chain fails. 
+		 * The computation may be aborted by the user, in this case, the
+		 * callback will return an interrupted error_code
+		 */
+
+		class abortable_computation {
+			private:
+				typedef boost::shared_ptr<abortable_function_base> abortable_function_ptr;
+				typedef std::vector<abortable_function_ptr> computation_seq;
+
+				typedef boost::function<void (const boost::system::error_code& e)> cb_type;
+
+				computation_seq seq;
+				cb_type cb;
+				size_t index;
+				bool user_ask_abort;
+
+				void handle_computation(const boost::system::error_code& e)
+				{
+					if (e) 
+						cb(e);
+					else {
+						if (user_ask_abort) 
+							return cb(make_error_code(boost::system::errc::interrupted));
+						if (index+1 == seq.size())
+							cb(boost::system::error_code());
+						else {
+							index++;
+							seq[index]->compute(boost::bind(
+										&abortable_computation::handle_computation,
+										this, boost::asio::placeholders::error));
+						}
+					}
+				}
+
+			public:
+				abortable_computation() {}
+
+				void push_back(abortable_function_base* fun) {
+					seq.push_back(abortable_function_ptr(fun));
+				}
+
+				void compute(cb_type cb_) {
+					cb = cb_;
+					index = 0;
+					user_ask_abort = false;
+
+					seq[index]->compute(boost::bind(&abortable_computation::handle_computation,
+										this, boost::asio::placeholders::error));
+				}
+
+				void abort() {
+					seq[index]->abort();
+					user_ask_abort = true;
+				}
+
+				void clear() { seq.clear(); }
+		};
+	};
+};
+
+#endif /* HYPER_MODEL_ABORTABLE_FUNCTION_HH_ */
---
+++ test/test_model_abortable_function.cc
@@ -0,0 +1,97 @@
+#include <iostream>
+
+#include <model/abortable_function.hh>
+#include <boost/test/unit_test.hpp>
+
+namespace {
+	void add(int& x, int& y, int& z, boost::function<void (const boost::system::error_code&)> cb)
+	{
+		z+= x + y;
+		cb(boost::system::error_code());
+	}
+
+	void times(int& x, int& y, int& z, boost::function<void (const boost::system::error_code&)> cb)
+	{
+		z+= x * y;
+		cb(boost::system::error_code());
+	}
+
+	void error_times(int& x, int& y, int& z, boost::function<void (const boost::system::error_code&)> cb)
+	{
+		z+= x * y;
+		cb(boost::system::error_code(42, boost::system::generic_category()));
+	}
+
+	struct test_callback
+	{
+		int x, y, z;
+		int handled_test;
+
+		hyper::model::abortable_computation computation;
+
+		void handle_second_test(const boost::system::error_code &e)
+		{
+			BOOST_CHECK(e);
+			BOOST_CHECK(x == 12);
+			BOOST_CHECK(y == 3);
+			BOOST_CHECK(z == 66); // the computation has stopped after error_times
+
+			handled_test++;
+		}
+
+		void handle_first_test(const boost::system::error_code &e)
+		{
+			BOOST_CHECK(!e);
+			BOOST_CHECK(x == 12);
+			BOOST_CHECK(y == 3);
+			BOOST_CHECK(z == 102);
+
+			z = 0;
+			computation.clear();
+			computation.push_back(new hyper::model::abortable_function(boost::bind(&add, 
+																	 boost::ref(x), boost::ref(y), 
+																	 boost::ref(z), _1)));
+			computation.push_back(new hyper::model::abortable_function(boost::bind(&add, 
+																	 boost::ref(x), boost::ref(y), 
+																	 boost::ref(z), _1)));
+			computation.push_back(new hyper::model::abortable_function(boost::bind(&error_times, 
+																	 boost::ref(x), boost::ref(y), 
+																	 boost::ref(z), _1)));
+			computation.push_back(new hyper::model::abortable_function(boost::bind(&times, 
+																	 boost::ref(x), boost::ref(y), 
+																	 boost::ref(z), _1)));
+
+			computation.compute(boost::bind(&test_callback::handle_second_test, this, _1));
+			handled_test++;
+		}
+
+		void test()
+		{
+			computation.push_back(new hyper::model::abortable_function(boost::bind(&add, 
+																	 boost::ref(x), boost::ref(y), 
+																	 boost::ref(z), _1)));
+			computation.push_back(new hyper::model::abortable_function(boost::bind(&add, 
+																	 boost::ref(x), boost::ref(y), 
+																	 boost::ref(z), _1)));
+			computation.push_back(new hyper::model::abortable_function(boost::bind(&times, 
+																	 boost::ref(x), boost::ref(y), 
+																	 boost::ref(z), _1)));
+			computation.push_back(new hyper::model::abortable_function(boost::bind(&times, 
+																	 boost::ref(x), boost::ref(y), 
+																	 boost::ref(z), _1)));
+			z = 0;
+			x = 12;
+			y = 3;
+
+			computation.compute(boost::bind(&test_callback::handle_first_test, this, _1));
+		}
+	};
+}
+
+BOOST_AUTO_TEST_CASE ( model_abortable_function_test )
+{
+	test_callback test;
+	test.handled_test = 0;
+	test.test();
+	BOOST_CHECK(test.handled_test == 2);
+}
