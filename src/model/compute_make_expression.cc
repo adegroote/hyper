ea7b4d0a37d884cf7c2e85532bb3fff3cc38384d
Arnaud Degroote <arnaud.degroote@laas.fr>
2011-04-11 16:46:22 +0200

    Add a first implementation of the make construction

---
+++ src/model/compute_make_expression.cc
@@ -0,0 +1,46 @@
+#include <model/ability.hh>
+#include <model/compute_make_expression.hh>
+
+namespace hyper {
+	namespace model {
+		compute_make_expression::compute_make_expression(
+			ability& a, const std::string& dst, const std::string& ctr, bool& res) : 
+			a(a), dst(dst), constraint(ctr), res(res), id(boost::none) {}
+
+		void compute_make_expression::handle_end_computation(
+				const boost::system::error_code& e,
+				cb_type cb)
+		{
+			if (e) 
+				cb(e);
+
+			res = ans.success;
+			cb(boost::system::error_code());
+		}
+
+		void compute_make_expression::compute(cb_type cb) 
+		{
+			id = boost::none;
+			rqst.constraint =  constraint;
+
+			id = a.client_db[dst].async_request(rqst, ans, 
+					boost::bind(&compute_make_expression::handle_end_computation,
+								this, boost::asio::placeholders::error, cb));
+		}
+
+		void compute_make_expression::handle_abort(const boost::system::error_code&)
+		{}
+
+		void compute_make_expression::abort() 
+		{
+			if (!id)
+				return;
+
+			abort_msg.id = *id;
+			a.client_db[dst].async_write(abort_msg, 
+						boost::bind(&compute_make_expression::handle_abort,
+									 this, boost::asio::placeholders::error));
+
+		}
+	}
+}
