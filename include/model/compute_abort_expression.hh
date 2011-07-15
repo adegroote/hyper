f33da093b4ce403c732a0f6141ba5bcdd897286c
Arnaud Degroote <arnaud.degroote@laas.fr>
2011-07-15 11:59:05 +0200

    [model] Implement code for abort expression

---
+++ include/model/compute_abort_expression.hh
@@ -0,0 +1,28 @@
+#ifndef HYPER_MODEL_COMPUTE_ABORT_EXPRESSION_HH_
+#define HYPER_MODEL_COMPUTE_ABORT_EXPRESSION_HH_
+
+#include <boost/system/error_code.hpp>
+#include <model/abortable_function.hh>
+#include <model/types.hh>
+
+namespace hyper {
+	namespace model {
+		struct ability;
+
+		class compute_abort_expression : public abortable_function_base {
+				ability& a;
+				const model::identifier& id;
+				network::abort abort_msg;
+
+				void handle_abort(const boost::system::error_code&);
+
+			public:
+				compute_abort_expression(ability&, const model::identifier&, boost::mpl::void_&);
+
+				void compute (cb_type cb);
+				bool abort();
+		};
+	}
+}
+
+#endif /* HYPER_MODEL_COMPUTE_ABORT_EXPRESSION_HH_ */
---
+++ src/model/compute_abort_expression.cc
@@ -0,0 +1,29 @@
+#include <model/ability.hh>
+#include <model/compute_abort_expression.hh>
+
+namespace hyper {
+	namespace model {
+		compute_abort_expression::compute_abort_expression(
+			ability& a, const model::identifier& id,
+			boost::mpl::void_& res) : 
+			a(a), id(id) {}
+
+		void compute_abort_expression::handle_abort(const boost::system::error_code&)
+		{}
+
+		void compute_abort_expression::compute(cb_type cb) 
+		{
+			abort_msg.src = a.name;
+			abort_msg.id = id.second;
+			a.client_db[id.first].async_write(abort_msg, 
+						boost::bind(&compute_abort_expression::handle_abort,
+									 this, boost::asio::placeholders::error));
+			cb(boost::system::error_code());
+		}
+
+		bool compute_abort_expression::abort() 
+		{
+			return false;
+		}
+	}
+}
