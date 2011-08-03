f9683ef1891ec299afe4dabacce49dc50cedc181
Arnaud Degroote <arnaud.degroote@laas.fr>
2011-08-03 10:42:35 +0200

    Workaround to help the compiler find the good overloading

--- src/compiler/recipe.cc
+++ src/compiler/recipe.cc
@@ -974,10 +974,16 @@ namespace hyper {
 			oss << "::" << exported_name() << "::pre_conditions::condition>\n";
 			generate_condition e_cond(oss, "pre", context_a, u);
 			std::for_each(pre.begin(), pre.end(), e_cond);
+
+			std::string base_type = "pre_conditions::remote_values::remote_vars_conf";
+
 			if (!pre_symbols.local_with_updater.empty())
 				oss << ", " << pre_symbols.local_list_variables_updated(next_next_indent);
-			if (!pre_symbols.remote.empty())
-				oss << ", " << pre_symbols.remote_list_variables(next_next_indent);
+			if (!pre_symbols.remote.empty()) {
+				oss << next_indent << ", " << base_type << "(\n";
+				oss << pre_symbols.remote_list_variables(next_next_indent);
+				oss << next_indent << ")\n";
+			}
 			oss << indent << ")" << std::endl;
 			}
 			oss << indent << "{" << std::endl;
