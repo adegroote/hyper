d08dd85f39e0059ccec6e8d1eea00c64394dc81d
Arnaud Degroote <arnaud.degroote@laas.fr>
2012-08-01 17:04:26 +0200

    [model] Add some way to evaluate assert expression

---
+++ include/model/compute_assert_expression.hh
@@ -0,0 +1,113 @@
+#ifndef HYPER_ASSERT_COMPUTE_ASSERT_EXPRESSION_HH_
+#define HYPER_ASSERT_COMPUTE_ASSERT_EXPRESSION_HH_
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/scoped_ptr.hpp>
+
+#include <model/abortable_function.hh>
+#include <model/types.hh>
+
+namespace hyper {
+	namespace model {
+		/** compute_assert_expression represents a computation represented by
+		 * #fun_ptry which waits until a certain predicate is true.
+		 * 
+		 * It is implemented as a loop test -> wait -> test -> wait ..., with a
+		 * configurable wait #delay_.
+		 */
+		class compute_assert_expression : public abortable_function_base {
+			private:
+				boost::scoped_ptr<abortable_function_base> fun_ptr; /**< 
+					function code to execute at each assert step */
+				logic::function_call f;
+
+				boost::asio::io_service& io_service_; /**< ref to io_service */
+				boost::posix_time::time_duration delay_; /**< wait time between two call of fun_ptr */
+				boost::asio::deadline_timer deadline_; /**< timer to wait #delay_ */
+				model::identifier& res;
+				bool& res_fun; /**< modified by a call to fun_ptr */
+
+				bool user_ask_abort; /**< true if abort has been called */
+				bool must_pause; /**< true if pause has been called, and not resumed */
+				bool running; /**< true if compute has not terminated */
+				bool waiting; /**< true if in the wait phase */
+				cb_type cb_; /**< store the final callback */
+				size_t idx_; /** < store the index of the assert, in the englobing computation */
+				bool first_call;
+
+				typedef boost::function<void (const boost::system::error_code&)> cb_type;
+
+				/**
+				 * Called when #deadline_ has expired
+				 * @param e is the return code of #deadline_::async_wait
+				 * @param cb is the final callback
+				 */
+				void handle_timeout(const boost::system::error_code& e, cb_type cb);
+
+				/**
+				 * Called when #fun_ptr->compute() has terminated()
+				 * @param e is the return code of compute
+				 * @param cb is the final callback
+				 */
+				void handle_computation(const boost::system::error_code& e, cb_type cb);
+
+				/**
+				 * Generic wait to handle the different error case. Deal with
+				 * the different possible states (pause, abort, ...)
+				 *
+				 * @return true if we have deal with an error case (and in this
+				 * case, the caller must stop any processing.
+				 * @return false otherwise (in the nominal case)
+				 */
+				bool handle_error(const boost::system::error_code& e, cb_type cb);
+
+			public:
+				/**
+				 * The constructor
+				 *
+				 * @param io_s is the io_service needed for boost::asio call
+				 * @param delay is the time between two tests call
+				 * @param fun_ptr represents the test function
+				 * @param f represents the computation done by fun_ptr, which
+				 * will be returned by error
+				 * @param res is the output of assert, it allows to furtherly stop it
+				 * @param res_fun is the bool where is the stored the result of
+				 * the computation fun_ptr
+				 * @param idx reprensents the index of the assert in its owner
+				 * computation
+				 */
+				compute_assert_expression(boost::asio::io_service& io_s,
+						boost::posix_time::time_duration delay,
+						abortable_function_base* fun_ptr,
+						const logic::function_call& f,
+						model::identifier& res, bool& res_fun, size_t idx);
+
+				/**
+				 * Launch the loop compute - wait
+				 * @param cb is the callback call when the action finishes
+				 */
+				void compute(cb_type cb);
+
+				/**
+				 * Abort the action.
+				 */
+
+				bool abort();
+
+				/**
+				 * Pause the current action
+				 */
+				void pause();
+
+				/**
+				 * Resume the current action
+				 */
+				void resume();
+
+				logic::expression error() const { return f; }
+		};
+	}
+}
+
+#endif /* HYPER_ASSERT_COMPUTE_ASSERT_EXPRESSION_HH_ */
---
+++ src/model/compute_assert_expression.cc
@@ -0,0 +1,116 @@
+#include <model/compute_assert_expression.hh>
+
+#include <boost/asio/placeholders.hpp>
+#include <boost/bind.hpp>
+
+namespace hyper {
+	namespace model {
+		compute_assert_expression::compute_assert_expression(boost::asio::io_service& io_s,
+				boost::posix_time::time_duration delay,
+				abortable_function_base* fun_ptr,
+				const logic::function_call& f,
+				model::identifier &res, bool& res_fun, size_t idx) :
+			fun_ptr(fun_ptr), f(f), io_service_(io_s), delay_(delay), deadline_(io_s),
+			res(res), res_fun(res_fun),
+			user_ask_abort(false), must_pause(false), running(false), waiting(false), 
+			idx_(idx), first_call(true)
+		{}
+
+		bool compute_assert_expression::handle_error(const boost::system::error_code& e, cb_type cb)
+		{
+			if (user_ask_abort) {
+				cb(make_error_code(boost::system::errc::interrupted));
+				running = false;
+				return true;
+			}
+
+			if (e) {
+				cb(e);
+				running = false;
+				return true;
+			}
+
+			return false;
+		}
+
+		void compute_assert_expression::handle_timeout(
+				const boost::system::error_code& e, cb_type cb)
+		{
+			waiting = false;
+			if (!handle_error(e, cb))
+				compute(cb);
+		}
+
+		void compute_assert_expression::handle_computation(
+				const boost::system::error_code& e, cb_type cb)
+		{
+			if (handle_error(e, cb)) return;
+
+			if (!res_fun) { 
+				running = false;
+				return cb(make_error_code(exec_layer_error::execution_ko));
+			}
+
+			if (must_pause)
+				return;
+
+			waiting = true;
+			deadline_.expires_from_now(delay_);
+			deadline_.async_wait(boost::bind(&compute_assert_expression::handle_timeout, 
+						this,
+						boost::asio::placeholders::error,
+						cb));
+		}
+
+		void compute_assert_expression::compute(cb_type cb)
+		{
+			running = true;
+			cb_ = cb;
+			if (must_pause)
+				return;
+
+			res.idx = idx_;
+
+			fun_ptr->compute(boost::bind(&compute_assert_expression::handle_computation, 
+										 this,
+										 boost::asio::placeholders::error,
+										 cb));
+
+			if (first_call) {
+				cb(boost::system::error_code());
+				first_call = false;
+			}
+		}
+
+		bool compute_assert_expression::abort()
+		{
+			if (!running)
+				return false;
+
+			user_ask_abort = true;
+
+			if (must_pause) {
+				running = false;
+				return waiting;
+			}
+
+			if (!waiting) 
+				fun_ptr->abort();
+
+			return true;
+		}
+
+		void compute_assert_expression::pause()
+		{
+			must_pause = true;
+		}
+
+		void compute_assert_expression::resume()
+		{
+			must_pause = false;
+			if (running) {
+				compute(cb_);
+			}
+		}
+	}
+}
