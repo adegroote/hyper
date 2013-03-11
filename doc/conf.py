83eb95848d3f50ec6e3deae5b8deb3ff0a7fa810
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-03-11 16:33:06 +0100

    [doc] Update copyright to mention CNRS

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'hyper'
 
 # General information about the project.
 project = 'hyper'
-copyright = '2013, Arnaud Degroote'
+copyright = '2013, Arnaud Degroote, LAAS-CNRS'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
