556ada19933d3bf585ba61e70640d4ecfb201c26
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-05-28 17:16:54 +0200

    [model] Add some test for compute_assert_expression

---
+++ test/test_model_compute_assert_expression.cc
@@ -0,0 +1,113 @@
+#include <iostream>
+
+#include <model/compute_assert_expression.hh>
+
+#include <boost/bind.hpp>
+#include <boost/variant.hpp>
+#include <boost/asio/placeholders.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+namespace {
+	struct test_function : public hyper::model::abortable_function_base {
+		bool res_;
+		size_t counter;
+
+		test_function() : res_(false), counter(0) {}
+
+		void compute(cb_type cb)
+		{
+			std::cout << "counter " << counter << std::endl;
+			++counter;
+			res_ = (counter < 42);
+			cb(boost::system::error_code());
+		}
+
+		bool abort() { return false; }
+
+		void pause() {}
+		void resume() {}
+
+		void reset() { res_ = false; counter = 0; }
+
+		bool& res() { return res_;}
+	};
+
+	struct test_compute_assert_expression {
+		test_function *fun_ptr;
+		hyper::model::abortable_computation* ptr; // owned by assert
+		hyper::model::compute_assert_expression assert_;
+		boost::asio::deadline_timer deadline;
+		hyper::model::identifier id;
+		int handled_test;
+		bool first_time;
+
+		test_compute_assert_expression(boost::asio::io_service& io_s):
+			fun_ptr(new test_function()), 
+			ptr(new hyper::model::abortable_computation()),
+			assert_(io_s, boost::posix_time::milliseconds(20), ptr,
+					hyper::logic::function_call("pipo"),
+					id, fun_ptr->res(), 0),
+			deadline(io_s)
+		{
+			ptr->push_back(fun_ptr);
+		}
+
+		void handle_timeout(const boost::system::error_code& e)
+		{
+			BOOST_CHECK(!e);
+			assert_.abort();
+		}
+
+		void handle_second_test(const boost::system::error_code& e)
+		{
+			BOOST_CHECK(e);
+			BOOST_CHECK(e == boost::system::errc::interrupted);
+			BOOST_CHECK(fun_ptr->res() == true);
+
+			handled_test++;
+		}
+
+		void handle_first_test(const boost::system::error_code& e)
+		{
+			if (first_time) {
+				BOOST_CHECK(!e);
+				first_time = false;
+			} else {
+				BOOST_CHECK(e);
+				BOOST_CHECK(fun_ptr->res() == false);
+				BOOST_CHECK(fun_ptr->counter == 42);
+
+				handled_test++;
+
+				fun_ptr->reset();
+				first_time = true;
+				/* Start the computing, but abort before its end */
+				assert_.compute(boost::bind(&test_compute_assert_expression::handle_second_test,
+										 this, _1));
+				deadline.expires_from_now(boost::posix_time::milliseconds(100));
+				deadline.async_wait(boost::bind(&test_compute_assert_expression::handle_timeout, 
+							this,
+							boost::asio::placeholders::error));
+			}
+		}
+
+		void async_test() 
+		{
+			first_time = true;
+			assert_.compute(boost::bind(&test_compute_assert_expression::handle_first_test,
+									 this, _1));
+		}
+	};
+}
+
+BOOST_AUTO_TEST_CASE ( model_compute_assert_expression_test )
+{
+	boost::asio::io_service io_s;
+	test_compute_assert_expression test(io_s);
+	test.handled_test = 0;
+	test.async_test();
+	io_s.run();
+
+	BOOST_CHECK(test.handled_test == 2);
+}
