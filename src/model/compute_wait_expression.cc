adegroote/hyper

commit a2a05e1dceb476db1f54250c5702c67ce73a3e2c

[model] Fix a race in pause / resume handling in wait_computation
The invariant running ^ waiting was not respected, and so the code was
possibly wrong. If the method is paused while waiting state (but still
in running before) and then resumed in the same state, the resume part
will call compute, which will call async_wait when it has finished. Two
async_wait on the same time lead to an ECANCELED on the first one, and
so the whole method is aborted.

src/model/compute_wait_expression.cc
@@ -52,6 +52,7 @@ namespace hyper {
 				return;
 
 			waiting = true;
+			running = false;
 			deadline_.expires_from_now(delay_);
 			deadline_.async_wait(boost::bind(&compute_wait_expression::handle_timeout, 
 						this,

commit c9c95ba97445aa8f0281877c3fe40ba17f0ec619

[model] Fix handling of abort in compute_wait_expression
The issue is a fallout from a2a05e1dceb. We need to check about active
state, i.e (running or waiting), and not only running.

src/model/compute_wait_expression.cc
@@ -75,7 +75,7 @@ namespace hyper {
 
 		bool compute_wait_expression::abort()
 		{
-			if (!running)
+			if (!(running or waiting))
 				return false;
 
 			user_ask_abort = true;