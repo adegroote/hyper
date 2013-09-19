d9e8c19f156e50960bf762987f6adf55438e36ae
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-09-19 07:29:47 +0200

    [model] Add an error() in compute_expression XXX
    Is it really correct ?

--- include/model/compute_expression.hh
+++ include/model/compute_expression.hh
@@ -14,9 +14,11 @@ namespace hyper {
 	
 			private:
 				updater_type updater;
+				bool is_updating;
 	
 				template <typename Handler, typename Ability, typename Local>
 				void handle_update(const boost::system::error_code& e,
+								   const network::error_context& err_ctx,
 								   const Ability& ability,
 								   const Local& local,
 								   Output& output,
@@ -26,7 +28,8 @@ namespace hyper {
 						boost::get<0>(handler)(e);
 						return;
 					}
-	
+
+					is_updating = false;
 					output = Fun()(updater, ability, local);
 					boost::get<0>(handler)(boost::system::error_code());
 				}
@@ -40,21 +43,36 @@ namespace hyper {
 				{
 					void (compute_expression::*f) (
 							const boost::system::error_code&e,
+							const network::error_context&,
 							const Ability& ,
 							const Local& ,
 							Output&,
 							boost::tuple<Handler>) =
 						&compute_expression::template handle_update<Handler, Ability, Local>;
+
+					is_updating = true;
 	
 					updater.async_update(
 							boost::bind(f, this, boost::asio::placeholders::error,
+												 _2, 
 												 boost::cref(a), 
 												 boost::cref(local),
 												 boost::ref(output),
 												 boost::make_tuple(handler)));
 				}
-		};
-	}
+
+				hyper::network::runtime_failure error() const 
+				{ 
+					hyper::network::runtime_failure f = 
+						hyper::network::execution_failure( 
+								Fun::logic_expression() 
+								);
+					if (is_updating) 
+						f.error_cause = updater.error();
+					return f;
+				}
+        };
+    }
 }
 
 #endif /* HYPER_MODEL_COMPUTE_EXPRESSION_HH_ */
