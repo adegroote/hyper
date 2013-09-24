3376ab71d6dc4ceae1225e8145887d73d569fa1d
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-09-24 09:34:48 +0200

    [compiler] Add a separate counter for abortable_computation
    Previously, in some situation, it is possible to generated malformed
    code, which create more than once variables with the same name.

--- include/compiler/recipe_expression_output.hh
+++ include/compiler/recipe_expression_output.hh
@@ -19,13 +19,16 @@ namespace hyper {
 			const symbolList& syms;
 			mutable size_t counter;
 			mutable size_t inner_var_counter;
+			mutable size_t computation_counter;
 
 			dump_recipe_expression(std::ostream& oss_, const universe& u_,
 								   const ability & a_, const task& t_,
 								   const symbolList& syms_, size_t counter = 0,
-								   size_t inner_var_counter = 0) : 
+								   size_t inner_var_counter = 0,
+								   size_t computation_counter = 0) : 
 				oss(oss_), u(u_), a(a_), t(t_), syms(syms_), counter(counter),
-				inner_var_counter(inner_var_counter)
+				inner_var_counter(inner_var_counter),
+				computation_counter(computation_counter)
 				{}
 
 			void operator() (const recipe_expression& r) const;
